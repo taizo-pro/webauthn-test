6fd4fb28ec56ac14fe0cbc79daa4aa8b61ef9440
taizo-pro <kazuki_harada@enpay.co.jp>
2024-12-15 11:08:38 +0900

    add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,26 +37,62 @@ export default function Home() {
 		try {
 			const credential = await navigator.credentials.create({
 				publicKey: {
+					// 署名の正当性を検証するためのランダムな文字列
+					// 攻撃者に入手されると公開鍵がすり替えられてしまうので、サーバで生成する
+					// 仕様では16文字以上の長さを持つランダムな文字列であることが推奨されている
 					challenge: new Uint8Array([1]),
+
+					// 認証サーバの情報
 					rp: {
-						name: "WebAuthn Test",
+						// RPの識別子。通常はドメイン名
 						id: window.location.hostname,
+						// RPの表示名
+						name: "WebAuthn Test",
 					},
+
+					// ユーザーの情報
 					user: {
+						// ユーザーの識別子
 						id: new Uint8Array(userIdArrayBuffer),
+						// ユーザー名。通常はメールアドレス
 						name: "test@example.com",
+						// ユーザーの表示名
 						displayName: "テストユーザー",
 					},
+
+					// 公開鍵のパラメータ
+					// 作成する公開鍵の種類とアルゴリズムを指定する配列
+					// algはアルゴリズムのID（例：-7 は ECDSA with SHA-256）
+					// ref. https://www.iana.org/assignments/cose/cose.xhtml#algorithms
+					// typeは"public-key"固定
+					// ref. https://developer.mozilla.org/ja/docs/Web/API/CredentialsContainer/create#pubkeycredparams
 					pubKeyCredParams: [
 						{ alg: -8, type: "public-key" },
 						{ alg: -7, type: "public-key" },
 					],
+
+					// 認証器の選択基準を指定する
 					authenticatorSelection: {
+						// 認証器がプラットフォームに内装されているか、クロスプラットフォームかを指定する
+						// ref. https://developer.mozilla.org/ja/docs/Web/API/CredentialsContainer/create#authenticatorattachment
+						authenticatorAttachment: "platform",
+
+						// ユーザーの認証が必要かどうかを指定する
+						// ref. https://developer.mozilla.org/ja/docs/Web/API/CredentialsContainer/create#userverification
 						userVerification: "required",
 					},
+
+					// PRF (Pseudo Random Function) 拡張機能
+					// 認証器から暗号鍵を導出するための設定
+          // ref. https://w3c.github.io/webauthn/#prf-extension
 					extensions: {
 						prf: {
+							// eval: 現在の認証器から直接鍵を導出
+							// evalByCredential: 特定の認証情報IDを指定して鍵を導出
 							eval: {
+								// PRF導出に使用するソルト値
+								// ソルトは暗号鍵の導出過程で使用される追加のランダムデータ
+								// 同じユーザー/認証器でも、異なるソルトを使うと異なる鍵が導出される
 								first: prfSalt,
 							},
 						},
